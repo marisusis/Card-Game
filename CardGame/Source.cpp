4e4aedc6035012a6c0aa0f03733f8977605af9dd
spacegeek224 <marisusisme@gmail.com>
2015-07-22 10:25:16 -0400

    Finished deckOfCards() declaration in for loops

--- CardGame/Source.cpp
+++ CardGame/Source.cpp
@@ -4,6 +4,25 @@
 
 int main() {
 
+	card deckOfCards[52];
+
+	for (int i=0; i<13; i++) {
+		deckOfCards[i].value = i + 2;
+		deckOfCards[i].suit = 'S';
+	}
+	for (int i=0; i<13; i++) {
+		deckOfCards[i + 13].value = i + 2;
+		deckOfCards[i + 13].suit = 'D';
+	}
+	for (int i=0; i<13; i++) {
+		deckOfCards[i + 26].value = i + 2;
+		deckOfCards[i + 26].suit = 'C';
+	}
+	for (int i=0; i<13; i++) {
+		deckOfCards[i + 39].value = i + 2;
+		deckOfCards[i + 39].suit = 'C';
+	}
+
 	return 0;
 
 }
