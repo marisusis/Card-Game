77a29779eaeec1ac171fe4c791ab1fa37a2ff85f
spacegeek224 <marisusisme@gmail.com>
2015-07-22 16:02:06 -0400

    Major changes

--- CardGame/Source.cpp
+++ CardGame/Source.cpp
@@ -1,62 +1,71 @@
 ﻿#include "card.h"
 #include <iostream>
+#include <fstream>
 
+void deckCardsOut();
 card deckOfCards[52];
-card player1Hand[5];
-	card player2Hand[5];
+card playerHand[5];
+card dealerHand[5];
 
 int main() {
-
+	#pragma execution_character_set("utf-8")
 	//_setmode(_fileno(stdout), _O_U16TEXT);
-    wprintf(L"\u2660\u2666");
-	wchar_t c3 = L'\u03a0';   // OK
+    //wprintf(L"\u2660\u2666");
+	/*wchar_t c3 = L'\u2660';   // OK
    wchar_t c4 = L'\u0642';
-   cout << c3 << c4 << endl;
+   u16string spade;
+   cout << spade;
+   cout << c3 << c4 << endl;*/
 	for (int i=0; i<13; i++) {
 		deckOfCards[i].value = i + 2;
-		deckOfCards[i].suit = '♠';
+		deckOfCards[i].suit = '\xE2\x99\xA0';
 	}
 	for (int i=0; i<13; i++) {
 		deckOfCards[i + 13].value = i + 2;
-		deckOfCards[i + 13].suit = '♦';
+		deckOfCards[i + 13].suit = 'D';
 	}
 	for (int i=0; i<13; i++) {
 		deckOfCards[i + 26].value = i + 2;
-		deckOfCards[i + 26].suit = '♣';
+		deckOfCards[i + 26].suit = 'C';
 	}
 	for (int i=0; i<13; i++) {
 		deckOfCards[i + 39].value = i + 2;
-		deckOfCards[i + 39].suit = '♥';
+		deckOfCards[i + 39].suit = 'H';
 	}
 
+	for (int i=0; i<53; i++) {
+		cout << deckOfCards[i];
+	}
 	shuffle(deckOfCards, 52);
 
 	
 
 	int topCard = 0;
 
-	int p1Size = 0;
-	int p2Size = 0;
-
-	for (int i = 0; i < 5; i++)
+	int pSize = 0;
+	int dSize = 0;
+	bool dealing = true;
+	while (dealing)
     {
-        player1Hand[i] = deckOfCards[topCard];
+		int i = 0;
+        playerHand[i] = deckOfCards[topCard];
         topCard++;
-        p1Size++;
+        pSize++;
  
-        player2Hand[i] = deckOfCards[topCard];
+        dealerHand[i] = deckOfCards[topCard];
         topCard++;
-        p2Size++;
+        dSize++;
+		i++;
     }
  
  
-    cout << "Player 1's Hand" << endl;
-    for (int i = 0; i < p1Size; i++)
+    cout << "Player's Hand" << endl;
+    for (int i = 0; i < dSize; i++)
     {
         cout << player1Hand[i].displayCard();
     }
-    cout << endl << "Player 2's Hand" << endl;
-    for (int i = 0; i < p2Size; i++)
+    cout << endl << "Dealer's Hand" << endl;
+    for (int i = 0; i < dSize; i++)
     {
         cout << player2Hand[i].displayCard();
     }
@@ -81,4 +90,9 @@ int main() {
 
 	return 0;
 
+}
+void deckCardsOut() {
+	for (int i=0; i<53; i++) {
+		cout << deckOfCards[i].displayCard;
+	}
 }
