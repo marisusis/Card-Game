9b67c60218f90b2651334e190cba7c03ce4ec68b
spacegeek224 <marisusisme@gmail.com>
2015-07-23 14:13:53 -0400

    Added back shuffle() and loadDeck() in card.h

--- CardGame/card.h
+++ CardGame/card.h
@@ -56,6 +56,34 @@ public:
 
 };
 //Shuffle a deck
+void shuffle(card deck[], int len) {
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < len; i++) {
+		int r = rand() % (len - i) + i;
+		card temp = deck[i];
+		deck[i] = deck[r];
+		deck[r] = temp;
+	}
+}
+//Generate a deck
+void loadDeck(card deck[52]) {
+	for (int i=0; i<13; i++) {
+		deck[i].value = i + 2;
+		deck[i].suit = '\x03';
+	}
+	for (int i=0; i<13; i++) {
+		deck[i + 13].value = i + 2;
+		deck[i + 13].suit = '\x04';
+	}
+	for (int i=0; i<13; i++) {
+		deck[i + 26].value = i + 2;
+		deck[i + 26].suit = '\x05';
+	}
+	for (int i=0; i<13; i++) {
+		deck[i + 39].value = i + 2;
+		deck[i + 39].suit = '\x06';
+	}
+}
 
 
 
